5e949b04f30c4efe7229d3a2490c52dc90df6a42
jeffs5 <jeffs5s@gmail.com>
2015-06-27 18:15:53 -0700

    Learning the IDE

--- Group-Alarm/app/src/main/java/com/test/stanford/group_alarm/MainActivity.java
+++ Group-Alarm/app/src/main/java/com/test/stanford/group_alarm/MainActivity.java
@@ -1,12 +1,15 @@
 package com.test.stanford.group_alarm;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +37,14 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.name_box);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
